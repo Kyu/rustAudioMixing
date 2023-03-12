0adfc2afced3a74a4b4cfe571082f933f63c19b3
Khubaib Ahmad <khubaib10@gmail.com>
2023-03-12 16:45:40 +0500

    Stereo case handled and clipping prevented. Output is always 44100 and bit depth 24 and stereo

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ extern crate hound;
 use std::f32::consts::PI;
 use hound::{SampleFormat, WavSpec, WavWriter};
 use std::collections::HashMap;
+use std::any::type_name;
 
 /**
  * This function opens a WAV file and returns a vector of samples
- * @param filePath - the path to the WAV file
+ * @param file_path - the path to the WAV file
  * @return a vector of samples
  */
-fn openWaveFile(filePath: &str) -> Vec<i16> {
+fn openWaveFile(file_path: &str) -> Vec<i32> {
     // open the WAV file
-    let file = match hound::WavReader::open(filePath) {
+    let file = match hound::WavReader::open(file_path) {
         Ok(f) => f,
         Err(e) => panic!("Failed to open WAV file: {}", e),
     };
@@ -23,22 +24,22 @@ fn openWaveFile(filePath: &str) -> Vec<i16> {
     let bit_depth = spec.bits_per_sample;
 
     //read the WAV file
-    let samples = file.into_samples::<i16>().collect::<Result<Vec<i16>, _>>().unwrap();
+    let samples = file.into_samples::<i32>().collect::<Result<Vec<i32>, _>>().unwrap();
 
     return samples;
 }
 
 /**
  * This function writes a vector of samples to a WAV file
- * @param filePath - the path to the WAV file
+ * @param file_path - the path to the WAV file
  * @param samples - a vector of samples
  * @param sample_rate - the sample rate of the WAV file
  * @param bit_depth - the bit depth of the WAV file
  * @param num_channels - the number of channels of the WAV file
  */
-fn writeWavFile(filePath: &str, samples: Vec<i16>, sample_rate: u32, bit_depth: u16, num_channels: u16) {
+fn write_wav_file(file_path: &str, samples: Vec<i32>, sample_rate: u32, bit_depth: u16, num_channels: u16) {
     // create a new WAV file
-    let mut writer = WavWriter::create(filePath, WavSpec {
+    let mut writer = WavWriter::create(file_path, WavSpec {
         channels: num_channels,
         sample_rate: sample_rate,
         bits_per_sample: bit_depth,
@@ -56,48 +57,54 @@ fn writeWavFile(filePath: &str, samples: Vec<i16>, sample_rate: u32, bit_depth: 
  * @param files - a vector of WAV file paths
  * @return a vector of samples
  */
-fn process_files(files: Vec<&str>) -> Vec<i16>
+fn process_files(files: Vec<&str>) -> Vec<i32>
 {
     //create hashmap to store samples with file name as keys
-    let mut filesData: HashMap<String, Vec<i16>> = HashMap::new();
-    let mut filesLength: HashMap<String, i32> = HashMap::new();
+    let mut files_data: HashMap<String, Vec<i32>> = HashMap::new();
+    let mut files_length: HashMap<String, i32> = HashMap::new();
 
     // Read each file and store its data in a HashMap along with its length
     for file in files {
         let filename = file.split('/').last().unwrap().split('.').next().unwrap().to_string();
-        filesData.insert(filename.clone(), openWaveFile(file));
-        filesLength.insert(filename.clone(), filesData.get(&filename).unwrap().len() as i32);
-        println!("{:?}", filesLength.get(&filename).unwrap());
+        files_data.insert(filename.clone(), openWaveFile(file));
+        files_length.insert(filename.clone(), files_data.get(&filename).unwrap().len() as i32);
+        println!("{:?}", files_length.get(&filename).unwrap());
     }
 
     // Determine the longest file and pad the shorter files with zeros
-    let mut longestLength = 0;
-    let mut longestFile = "".to_string();
-    for (key, value) in filesLength.iter() {
-        if *value > longestLength {
-            longestLength = *value;
-            longestFile = key.to_string();
+    let mut longest_length = 0;
+    let mut longest_file = "".to_string();
+    for (key, value) in files_length.iter() {
+        if *value > longest_length {
+            longest_length = *value;
+            longest_file = key.to_string();
         }
     }
-    for (key, value) in filesLength.iter() {
-        if *value < longestLength {
-            let mut samples = filesData.get(&key.to_string()).unwrap().to_vec();
+    for (key, value) in files_length.iter() {
+        if *value < longest_length {
+            let mut samples = files_data.get(&key.to_string()).unwrap().to_vec();
             let mut i = 0;
-            while i < longestLength - *value {
+            while i < longest_length - *value {
                 samples.push(0);
                 i += 1;
             }
-            filesData.insert(key.to_string(), samples);
+            files_data.insert(key.to_string(), samples);
         }
     }
 
-    // Combine the samples from each file and calculate the average
-    let mut samples = filesData.get(&longestFile).unwrap().to_vec();
-    for (key, value) in filesData.iter() {
-        if key != &longestFile {
+    // Combine the samples from each file and calculate the average and normalize the loudness
+    let mut samples = files_data.get(&longest_file).unwrap().to_vec();
+    for (key, value) in files_data.iter() {
+        if key != &longest_file {
             let mut i = 0;
             while i < value.len() {
-                samples[i] = (samples[i] + value[i]) / 2;
+                samples[i] = (samples[i] + value[i]);
+                // prevent clipping by limiting the sample to 24 bits
+                if samples[i] > 8388607 {
+                    samples[i] = 8388580;
+                } else if samples[i] < -8388608 {
+                    samples[i] = -8388580;
+                }
                 i += 1;
             }
         }
@@ -108,8 +115,8 @@ fn process_files(files: Vec<&str>) -> Vec<i16>
 
 fn main() {
     // add file path here
-    let files = vec!["src/test.wav", "src/test3.wav", "src/test2.wav"];
+    let files = vec!["src/organ_EM_120.wav", "src/perc_AM_120.wav", "src/piano_AM_120.wav", "src/riser_AM_120.wav", "src/shakers_AM_120.wav", "src/synth_AM_120.wav"];
 
     //write samples to new file
-    writeWavFile("src/out.wav", process_files(files), 48000, 16, 1);
+    write_wav_file("src/out.wav", process_files(files), 44100, 24, 2);
 }
